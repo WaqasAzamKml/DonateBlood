78670ba3a84b7cc2e11e3ef8ff1747c14fa3aeb0
Muhammad Waqas <waqasazamkml@gmail.com>
2016-11-21 15:36:25 +0500

    Integrated Firbase & Implemented FirebaseInstanceIDService to generate Device token. Token will be saved later.

---
+++ app/src/main/java/net/itempire/donateblood/MyFirebaseInstanceIDService.java
@@ -0,0 +1,22 @@
+package net.itempire.donateblood;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by MIRSAAB on 11/16/2016.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("DonateBloodToken", "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        // sendRegistrationToServer(refreshedToken);
+    }
+}
